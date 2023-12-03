c5839c89034076e765437b64e6480df92ace0fe4
daortellado <daortellado@gmail.com>
2023-12-03 14:03:56 -0800

    testing game list

--- app.js
+++ app.js
@@ -163,7 +163,7 @@ app.post("/login", (request, response) => {
 
 // free endpoint
 app.get("/free-endpoint", (request, response) => {
-  response.json({ message: "You are free to access me anytime" });
+  response.json({ message: Video.game });
 });
 
 // authentication endpoint
