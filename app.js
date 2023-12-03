e2abab437d0d6c38a12a55c08c28a40b5d41734f
daortellado <daortellado@gmail.com>
2023-12-03 11:59:51 -0800

    cors

--- app.js
+++ app.js
@@ -12,19 +12,11 @@ const auth = require("./auth");
 // execute database connection
 dbConnect();
 
-// Curb Cores Error by adding a header here
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
-});
+  });
 
 // body parser configuration
 app.use(bodyParser.json());
