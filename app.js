ec224034e6ecd7bf9e555676d098380f513a9934
daortellado <daortellado@gmail.com>
2023-12-03 14:42:38 -0800

    test

--- app.js
+++ app.js
@@ -168,7 +168,7 @@ app.get("/free-endpoint", (request, response) => {
 
 // authentication endpoint
 app.get("/auth-endpoint", auth, (request, response) => {
-  const videocontents = Videos;
+  const videocontents = "test";
   response.send({ message: "Select a game below" });
   response.send({ videolist: videocontents });
 });
