a7048ef2bc352c96a6a4ab589a9ebcea42cd1974
daortellado <daortellado@gmail.com>
2023-12-03 13:20:08 -0800

    add video model

---
+++ db/videoModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+// video schema
+const VideoSchema = new mongoose.Schema({
+  // name field
+  name: {
+    type: String,
+    required: [true, "Please provide an video name"],
+    unique: [false],
+  },
+    // game field
+    name: {
+        type: String,
+        required: [true, "Please select a game"],
+        unique: [false],
+      },
+  //   password field
+  link: {
+    type: String,
+    required: [true, "Please provide a video link"],
+    unique: false,
+  },
+});
+
+// export VideoSchema
+module.exports = mongoose.model.Videos || mongoose.model("Videos", VideoSchema);
