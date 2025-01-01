8115ace2aecd48ceb9d65610b3569fbac01457a5
daortellado <daortellado@gmail.com>
2025-01-01 13:23:58 -0800

    changing the video query to accommodate seasons

--- controllers/video.controller.js
+++ controllers/video.controller.js
@@ -3,9 +3,15 @@ const router = express.Router()
 const Video = require("../db/videoModel");
 
 router.get('/', (req, res) => {
-  Video.find({ season: "current" })
-  .then(data => res.send(data))
-  .catch(err => console.log(err))
-})
+    const { season } = req.query;
+    const query = season ? { season } : {};
+    
+    Video.find(query)
+    .then(data => res.send(data))
+    .catch(err => {
+        console.log(err);
+        res.status(500).send(err);
+    })
+});
 
-  module.exports = router
+module.exports = router;
